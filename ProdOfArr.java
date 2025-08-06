0432153b583e7122c83539cd15eb0761059f8ea1
DPEDITS <debashishparida75@gmail.com>
2025-08-06 19:31:42 +0530

    Completed my Product of Array problem done

--- ProdOfArr.java
+++ ProdOfArr.java
@@ -1,3 +1,11 @@
+//LeetCode 238. Product of Array Except Self
+// Title: Product of Array Except Self
+// This problem is about calculating the product of all elements in an array except for the element at the current index, without using division and in O(n) time complexity.
+//Example 1:
+// Input: [1,2,3,4]
+// Output: [24,12,8,6]
+//Time Complexity: O(n)
+//Space Complexity: O(n)
 public class ProdOfArr {
         public int[] productExceptSelf(int[] nums) {
             int n = nums.length;
