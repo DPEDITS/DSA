abfe8b8ff5e4e3792606ae97b6a53808cc4932ec
DPEDITS <debashishparida75@gmail.com>
2025-08-07 19:59:28 +0530

    Completed LCS

--- LCS.java
+++ LCS.java
@@ -1,5 +1,12 @@
 //Leetcode Problem:128 Longest Consecutive Sequence
 // Title: Longest Consecutive Sequence
+// // This problem is about finding the length of the longest consecutive elements sequence in an unsorted array.
+ // Example 1:
+ // Input: nums = [100,4,200,1,3,2]
+ // Output: 4
+ // Explanation: The longest consecutive elements sequence is [1, 2, 3, 4]. Therefore its length is 4.
+ // Time Complexity: O(n2) where n is the number of elements in the input array.
+ // Space Complexity: O(n) for storing the elements in a HashSet.
 import java.util.*;
 
 public class LCS {
