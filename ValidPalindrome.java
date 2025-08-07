4976dd2bbb7811376c7184689beb149fca75c527
DPEDITS <debashishparida75@gmail.com>
2025-08-07 20:22:22 +0530

    Completed Valid Palindrome

--- ValidPalindrome.java
+++ ValidPalindrome.java
@@ -1,3 +1,11 @@
+//LeetCode Problem:125 Valid Palindrome
+// Title: Valid Palindrome
+// This problem is about determining if a given string is a valid palindrome, considering only alphanumeric characters and ignoring case.
+//Example 1:
+//Input: s = "Was it a car or a cat I saw?"
+//Output: true
+//Time Complexity: O(n) where n is the length of the input string.
+//Space Complexity: O(n) for storing the filtered characters in a StringBuilder.
 public class ValidPalindrome {
 
         public boolean isPalindrome(String s) {
