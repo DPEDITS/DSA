0426963cece4d0a3ba5a4c3a20f47d9b42fec9a3
DPEDITS <debashishparida75@gmail.com>
2025-08-07 20:18:14 +0530

    Completed Valid Palindrome

---
+++ ValidPalindrome.java
@@ -0,0 +1,13 @@
+public class ValidPalindrome {
+
+        public boolean isPalindrome(String s) {
+            StringBuilder newStr = new StringBuilder();
+            for (char c : s.toCharArray()) {
+                if (Character.isLetterOrDigit(c)) {
+                    newStr.append(Character.toLowerCase(c));
+                }
+            }
+            return newStr.toString().equals(newStr.reverse().toString());
+        }
+    }
+
