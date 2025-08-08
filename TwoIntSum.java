41f96bf72d6c5e51cce389055b785d27bc66fb2e
DPEDITS <debashishparida75@gmail.com>
2025-08-08 22:10:31 +0530

    Completed Two Integer Sum

--- TwoIntSum.java
+++ TwoIntSum.java
@@ -1,3 +1,13 @@
+//LeetCode Problem: Two Sum II - Input Array Is Sorted
+// Title: Two Sum II - Input Array Is Sorted
+// This problem is about finding two numbers in a sorted array that add up to a specific target.
+// Example 1:
+// Input: numbers = [2,7,11,15], target = 9
+// Output: [1,2]
+// Time Complexity: O(n^2) where n is the length of the input array.
+// Space Complexity: O(1) since we are not using any additional data structures that grow with input size.
+//Technique: Brute Force
+// This solution uses a brute force approach to find the two indices of the numbers that sum up to the target.
 public class TwoIntSum {
         public int[] twoSum(int[] numbers, int target) {
             for (int i = 0; i < numbers.length; i++) {
