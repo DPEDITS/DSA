6d8c187531c983d0d6fb01897f95bfa282977a9e
DPEDITS <debashishparida75@gmail.com>
2025-08-09 22:43:39 +0530

    Completed Container With Most Water

--- ContWithMostWater.java
+++ ContWithMostWater.java
@@ -1,3 +1,12 @@
+//Leetcode Problem:11. Container With Most Water
+// Title: Container With Most Water
+// This problem is about finding two lines that together with the x-axis form a container that can hold the most water.
+// Example 1:
+// Input: height = [1,8,6,2,5,4,8,3,7]
+// Output: 49
+// Time Complexity: O(n^2) where n is the length of the input array.
+// Space Complexity: O(1) since we are using a constant amount of space for variables.
+//// Technique: Brute Force
 public class ContWithMostWater {
         public int maxArea(int[] heights) {
             int res = 0;
