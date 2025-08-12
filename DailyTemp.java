abc274f869c3153aa8e3bc43dea0a7cc9e5ae15b
DPEDITS <debashishparida75@gmail.com>
2025-08-12 23:05:11 +0530

    Completed DailyTemp.java

--- DailyTemp.java
+++ DailyTemp.java
@@ -1,4 +1,13 @@
-
+//LeetCode Problem: 739. Daily Temperatures
+// Link: https://leetcode.com/problems/daily-temperatures/description/
+//// Given an array of integers temperatures represents the daily temperatures, return an array answer such that answer[i] is the number of days you have to wait after the ith day to get a warmer temperature. If there is no future day for which this is possible, keep answer[i] == 0 instead.
+/// // Example 1:
+///    // Input: temperatures = [73,74,75,71,69,72,76,73]
+/// //    // Output: [1,1,4,2,1,1,0,0]
+/// time complexity: O(n), where n is the length of the temperatures array
+/// // space complexity: O(n), where n is the length of the temperatures array
+/// // Approach: Use a stack to keep track of the indices of the temperatures.
+/// Technique: Monotonic Stack
 import java.util.*;
 public class DailyTemp {
         public int[] dailyTemperatures(int[] temperatures) {
